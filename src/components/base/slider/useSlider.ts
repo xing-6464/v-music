93f358a4a3120700d26618b3e1e85b211e16a217
xing-6464 <xing_6464@foxmail.com>
2023-10-03 23:56:33 +0800

    轮播图组件的开发

---
+++ src/components/base/slider/useSlider.ts
@@ -0,0 +1,35 @@
+import type { Ref } from 'vue'
+import { onMounted, onUnmounted, ref } from 'vue'
+
+import BScroll from '@better-scroll/core'
+import Slide from '@better-scroll/slide'
+import type { BScrollConstructor } from '@better-scroll/core/dist/types/BScroll'
+
+BScroll.use(Slide)
+
+export default function useSlider(wrapperRef: Ref<HTMLElement | null>) {
+  const slider = ref<BScrollConstructor>()
+  const currentPageIndex = ref(0)
+
+  onMounted(() => {
+    const sliderValue = (slider.value = new BScroll(wrapperRef.value!, {
+      click: true,
+      scrollX: true,
+      scrollY: false,
+      momentum: false,
+      bounce: false,
+      probeType: 2,
+      slide: true,
+    }))
+
+    sliderValue.on('slideWillChange', (page: { pageX: number }) => {
+      currentPageIndex.value = page.pageX
+    })
+  })
+
+  onUnmounted(() => {
+    slider.value?.destroy()
+  })
+
+  return { slider, currentPageIndex }
+}

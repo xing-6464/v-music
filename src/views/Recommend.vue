c53faa10f56982f70cc1aedc62d902b55ffefd51
xing-6464 <xing_6464@foxmail.com>
2023-10-04 00:05:04 +0800

    轮播图组件的使用

--- src/views/Recommend.vue
+++ src/views/Recommend.vue
@@ -1,17 +1,48 @@
 <template>
   <div class="recommend">
-    推荐页面
+    <div class="slider-wrapper">
+      <div class="slider-content">
+        <slider v-if="sliders.length" :sliders="sliders"></slider>
+      </div>
+    </div>
   </div>
 </template>
 
 <script setup lang="ts">
-import { onMounted } from 'vue'
-import { getRecommend } from '../service/recommend';
+import { onMounted, ref } from 'vue'
+import { getRecommend } from '../service/recommend'
+import Slider from '../components/base/slider/Slider.vue'
+
+const sliders = ref<any[]>([])
 
 onMounted(async () => {
   const result = await getRecommend()
   console.log(result)
+  sliders.value = result.sliders
 })
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.recommend {
+  position: fixed;
+  width: 100%;
+  top: 88px;
+  bottom: 0;
+
+  .slider-wrapper {
+    position: relative;
+    width: 100%;
+    height: 0;
+    padding-top: 40%;
+    overflow: hidden;
+
+    .slider-content {
+      position: absolute;
+      left: 0;
+      top: 0;
+      width: 100%;
+      height: 100%;
+    }
+  }
+}
+</style>

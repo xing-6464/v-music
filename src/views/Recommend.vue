e2e5fe0b90c9e2c844e544f745f2796b6e56d0ca
xing-6464 <xing_6464@foxmail.com>
2023-10-04 01:43:57 +0800

    歌单列表实现

--- src/views/Recommend.vue
+++ src/views/Recommend.vue
@@ -5,6 +5,24 @@
         <slider v-if="sliders.length" :sliders="sliders"></slider>
       </div>
     </div>
+    <div class="recommend-list">
+      <h1 class="list-title">热门歌单列表</h1>
+      <ul>
+        <li v-for="item in albums" class="item" :key="item.id">
+          <div class="icon">
+            <img :src="item.pic" width="60" height="60">
+          </div>
+          <div class="text">
+            <h2 class="name">
+              {{ item.username }}
+            </h2>
+            <p class="title">
+              {{ item.title }}
+            </p>
+          </div>
+        </li>
+      </ul>
+    </div>
   </div>
 </template>
 
@@ -13,12 +31,18 @@ import { onMounted, ref } from 'vue'
 import { getRecommend } from '../service/recommend'
 import Slider from '../components/base/slider/Slider.vue'
 
-const sliders = ref<any[]>([])
+import type { Sliders, Albums } from './types'
+
+// 轮播图数据
+const sliders = ref<Sliders[]>([])
+// 歌单数据
+const albums = ref<Albums[]>([])
 
 onMounted(async () => {
   const result = await getRecommend()
   console.log(result)
   sliders.value = result.sliders
+  albums.value = result.albums
 })
 </script>
 
@@ -28,6 +52,7 @@ onMounted(async () => {
   width: 100%;
   top: 88px;
   bottom: 0;
+  overflow: scroll;
 
   .slider-wrapper {
     position: relative;
@@ -42,6 +67,48 @@ onMounted(async () => {
       top: 0;
       width: 100%;
       height: 100%;
+    }
+  }
+
+  .recommend-list {
+    .list-title {
+      height: 65px;
+      line-height: 65px;
+      text-align: center;
+      font-size: $font-size-medium;
+      color: $color-theme;
+    }
+
+    .item {
+      display: flex;
+      box-sizing: border-box;
+      align-items: center;
+      padding: 0 20px 20px 20px;
+
+      .icon {
+        flex: 0 0 60px;
+        width: 60px;
+        padding-right: 20px;
+      }
+
+      .text {
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        flex: 1;
+        line-height: 20px;
+        overflow: hidden;
+        font-size: $font-size-medium;
+      }
+
+      .name {
+        margin-bottom: 10px;
+        color: $color-text;
+      }
+
+      .title {
+        color: $color-text-d;
+      }
     }
   }
 }
---
+++ src/views/types.ts
@@ -0,0 +1,7 @@
+export type Sliders = { id: string; pic: string; link: string }
+export type Albums = {
+  id: string
+  pic: string
+  title: string
+  username: string
+}

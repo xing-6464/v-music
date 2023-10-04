1c68cc6d703f71d425769a96f855acc1793617bf
xing-6464 <xing_6464@foxmail.com>
2023-10-04 23:01:37 +0800

    图片懒加载的实现

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,7 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import lazyPlugin from 'vue3-lazy'
+import defaultImg from './assets/images/default.png'
 
 import App from './App.vue'
 import router from './router'
@@ -10,5 +12,9 @@ const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
+
+app.use(lazyPlugin, {
+  loading: defaultImg,
+})
 
 app.mount('#app')

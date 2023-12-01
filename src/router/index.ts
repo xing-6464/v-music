92f399e404af3991ce80f551de43fb40e82afd26
xing-6464 <xing_6464@foxmail.com>
2023-12-01 12:45:33 +0800

    路由组件异步加载

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,12 +1,20 @@
 import { createRouter, createWebHashHistory } from 'vue-router'
-import Recommend from '../views/Recommend.vue'
-import Singer from '../views/Singer.vue'
-import TopList from '../views/TopList.vue'
-import Search from '../views/Search.vue'
-import SingerDetail from '../views/SingerDetail.vue'
-import Album from '@/views/Album.vue'
-import TopDetail from '@/views/TopDetail.vue'
-import UserCenter from '@/views/UserCenter.vue'
+const Recommend = () => import('@/views/Recommend.vue')
+const Singer = () => import('@/views/Singer.vue')
+const TopList = () => import('@/views/TopList.vue')
+const Search = () => import('@/views/Search.vue')
+const SingerDetail = () => import('@/views/SingerDetail.vue')
+const Album = () => import('@/views/Album.vue')
+const TopDetail = () => import('@/views/TopDetail.vue')
+const UserCenter = () => import('@/views/UserCenter.vue')
+// import Recommend from '../views/Recommend.vue'
+// import Singer from '../views/Singer.vue'
+// import TopList from '../views/TopList.vue'
+// import Search from '../views/Search.vue'
+// import SingerDetail from '../views/SingerDetail.vue'
+// import Album from '@/views/Album.vue'
+// import TopDetail from '@/views/TopDetail.vue'
+// import UserCenter from '@/views/UserCenter.vue'
 
 const router = createRouter({
   history: createWebHashHistory(import.meta.env.BASE_URL),
